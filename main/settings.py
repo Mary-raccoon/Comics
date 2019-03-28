84cb71565f43509d214e03e1c219ad8d85602b2e
Mary-raccoon <36716699+Mary-raccoon@users.noreply.github.com>
2019-03-27 20:21:17 -0700

    commit 5

--- main/settings.py
+++ main/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'vi$d+)y1frs#p*^%gc54g79e%a4no1s4s8n&cq$mpd+i9l^9*+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['3.17.206.71']
 
 
 # Application definition
