0115dcb72ef02635d36c08d2fcaeee17198793d6
Mary Raccoon <maryraccoon@yahoo.com>
2019-04-04 14:13:57 -0700

    commit 23

--- main/settings.py
+++ main/settings.py
@@ -138,7 +138,7 @@ AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_LOCATION = 'static'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'comic/static'),
 ]
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
