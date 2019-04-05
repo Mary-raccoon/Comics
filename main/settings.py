6f522dae0593fdeb76e44b183736a5d5d46d3a77
Mary Raccoon <maryraccoon@yahoo.com>
2019-04-04 18:06:33 -0700

    commit 26

--- main/settings.py
+++ main/settings.py
@@ -129,6 +129,7 @@ USE_TZ = False
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
