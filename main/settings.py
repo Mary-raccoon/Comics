d2407ae98431accea9ae99491bdab989ad9150af
Mary Raccoon <maryraccoon@yahoo.com>
2019-04-09 01:16:50 -0700

    updated settings static

--- main/settings.py
+++ main/settings.py
@@ -130,7 +130,7 @@ USE_TZ = False
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'apps/comic/static/media')
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'apps/comic/staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
