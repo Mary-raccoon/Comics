4584d7ffad6b4c01910bf824c34109777c4f427b
Mary Raccoon <maryraccoon@yahoo.com>
2019-04-09 17:39:50 -0700

    updated templates

--- main/settings.py
+++ main/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
 
 DEBUG = True
-# ALLOWED_HOSTS = ['comicsmanage.herokuapp.com']
+ALLOWED_HOSTS = ['comicsmanage.herokuapp.com']
 # Application definition
 
 INSTALLED_APPS = [
